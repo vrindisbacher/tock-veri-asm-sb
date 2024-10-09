1f1ccf5bbb7a1773843f3597c4d7ce147f4a3d7a
vrindisbacher <vrindisbacher77@gmail.com>
2024-10-08 18:41:30 -0700

    Add MMode class for arm dissasembly so it can decode msr instr

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,22 @@
 use std::io::Read;
 
 use capstone::{
-    arch::{self, BuildsCapstone, BuildsCapstoneEndian},
-    Capstone,
+    arch::{self, BuildsCapstone},
+    Capstone, Endian,
 };
-use elf::{endian::AnyEndian, section::SectionHeader, ElfBytes};
+use elf::{endian::AnyEndian, ElfBytes};
 
-fn unasm(cs: Capstone, buf: &[u8]) {
+fn unasm(cs: Capstone, buf: &[u8], section_name: &str) {
     let insns = cs.disasm_all(buf, 0x0).expect("Failed to disassemble");
-    println!("Found {} instructions", insns.len());
+    if insns.len() > 0 {
+        println!("\nDisassembly for section {}:\n", section_name);
+    }
     for i in insns.as_ref() {
-        // println!("{}", i);
         match cs.insn_name(i.id()) {
             None => (),
             Some(name) => {
                 let op = i.op_str().unwrap_or("");
-                println!("{} {}", name, op);
+                println!("\t{} {}", name, op);
             }
         }
     }
@@ -28,20 +29,20 @@ fn read_file_to_bytes(path: &str) -> Vec<u8> {
     buf
 }
 
-fn arm_capstone() -> Capstone {
-    Capstone::new()
-        .arm()
-        .mode(arch::arm::ArchMode::Thumb)
-        .endian(capstone::Endian::Little)
-        .build()
-        .unwrap()
+fn arm_mclass_capstone() -> Capstone {
+    Capstone::new_raw(
+        capstone::Arch::ARM,
+        capstone::Mode::Thumb,
+        vec![capstone::ExtraMode::MClass].into_iter(),
+        Some(Endian::Little),
+    )
+    .unwrap()
 }
 
 fn riscv_capstone() -> Capstone {
     Capstone::new()
         .riscv()
         .mode(arch::riscv::ArchMode::RiscV32)
-        .endian(capstone::Endian::Little)
         .build()
         .unwrap()
 }
@@ -49,24 +50,39 @@ fn riscv_capstone() -> Capstone {
 fn unasm_arm() {
     println!("ARM");
     let arm_prgm = read_file_to_bytes("./arm_assem_example.o");
-    let arm_cs = arm_capstone();
-    unasm(arm_cs, &arm_prgm);
+    let file = ElfBytes::<AnyEndian>::minimal_parse(&arm_prgm).expect("Failed to parse arm");
+    let all_headers = file
+        .section_headers_with_strtab()
+        .expect("Expected section headers");
+    let header_names = all_headers.1.expect("Expected header names");
+    let headers = all_headers.0.expect("Expected headers");
+    for header in headers {
+        let (data, _) = file
+            .section_data(&header)
+            .expect("Couldn't get section data");
+        let arm_cs = arm_mclass_capstone();
+        let section_name = header_names.get(header.sh_name as usize).expect("Could not find header name");
+        unasm(arm_cs, data, section_name);
+    }
 }
 
 fn unasm_riscv() {
     println!("\nRISC-V");
     let riscv_prgm = read_file_to_bytes("./riscv_assem_example.o");
     let file = ElfBytes::<AnyEndian>::minimal_parse(&riscv_prgm).expect("Failed to parse riscv");
-    let trap_section_header: SectionHeader = file
-        .section_header_by_name(".riscv.trap")
-        .expect("section table should be parseable")
-        .expect("file should have a .riscv.trap section");
-
-    let (data, _) = file
-        .section_data(&trap_section_header)
-        .expect("Couldn't get section data");
-    let riscv_cs = riscv_capstone();
-    unasm(riscv_cs, data);
+    let all_headers = file
+        .section_headers_with_strtab()
+        .expect("Expected section headers");
+    let header_names = all_headers.1.expect("Expected header names");
+    let headers = all_headers.0.expect("Expected headers");
+    for header in headers {
+        let (data, _) = file
+            .section_data(&header)
+            .expect("Couldn't get section data");
+        let riscv_cs = riscv_capstone();
+        let section_name = header_names.get(header.sh_name as usize).expect("Could not find header name");
+        unasm(riscv_cs, data, section_name);
+    }
 }
 
 fn main() {
