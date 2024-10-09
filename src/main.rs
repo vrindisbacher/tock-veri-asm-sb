ca84f217f18f6beee6855239c3474c2c5eee5afd
vrindisbacher <vrindisbacher77@gmail.com>
2024-10-08 18:41:30 -0700

    Add MMode class for arm dissasembly so it can decode msr instr

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,19 @@
 use std::io::Read;
 
 use capstone::{
-    arch::{self, BuildsCapstone, BuildsCapstoneEndian},
-    Capstone,
+    arch::{self, BuildsCapstone},
+    Capstone, Endian,
 };
-use elf::{endian::AnyEndian, section::SectionHeader, ElfBytes};
+use elf::{
+    endian::{AnyEndian, LittleEndian},
+    section::SectionHeader,
+    ElfBytes,
+};
 
 fn unasm(cs: Capstone, buf: &[u8]) {
     let insns = cs.disasm_all(buf, 0x0).expect("Failed to disassemble");
     println!("Found {} instructions", insns.len());
     for i in insns.as_ref() {
-        // println!("{}", i);
         match cs.insn_name(i.id()) {
             None => (),
             Some(name) => {
@@ -28,20 +31,20 @@ fn read_file_to_bytes(path: &str) -> Vec<u8> {
     buf
 }
 
-fn arm_capstone() -> Capstone {
-    Capstone::new()
-        .arm()
-        .mode(arch::arm::ArchMode::Thumb)
-        .endian(capstone::Endian::Little)
-        .build()
-        .unwrap()
+fn arm_mclass_capstone() -> Capstone {
+    Capstone::new_raw(
+        capstone::Arch::ARM,
+        capstone::Mode::Thumb,
+        vec![capstone::ExtraMode::MClass].into_iter(),
+        Some(Endian::Little),
+    )
+    .unwrap()
 }
 
 fn riscv_capstone() -> Capstone {
     Capstone::new()
         .riscv()
         .mode(arch::riscv::ArchMode::RiscV32)
-        .endian(capstone::Endian::Little)
         .build()
         .unwrap()
 }
@@ -49,8 +52,17 @@ fn riscv_capstone() -> Capstone {
 fn unasm_arm() {
     println!("ARM");
     let arm_prgm = read_file_to_bytes("./arm_assem_example.o");
-    let arm_cs = arm_capstone();
-    unasm(arm_cs, &arm_prgm);
+    let file = ElfBytes::<AnyEndian>::minimal_parse(&arm_prgm).expect("Failed to parse arm");
+    let isr_section_header: SectionHeader = file
+        .section_header_by_name(".generic_isr_arm_v7m")
+        .expect("section table should be parseable")
+        .expect("file should have a .generic_isr_arm_v7m section");
+
+    let (data, _) = file
+        .section_data(&isr_section_header)
+        .expect("Couldn't get section data");
+    let arm_cs = arm_mclass_capstone();
+    unasm(arm_cs, data);
 }
 
 fn unasm_riscv() {
