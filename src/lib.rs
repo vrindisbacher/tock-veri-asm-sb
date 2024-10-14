37ae875285e7732ff17d6a6bc67cebd876f7449d
vrindisbacher <vrindisbacher77@gmail.com>
2024-10-14 10:39:38 -0700

    Modifications based off dissasembly

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,33 @@ pub mod armv7m;
 
 use armv7m::cpu::{Armv7m, GeneralPurposeRegister, SpecialRegister, Value};
 
+
+//
+// Here is disassembly of the armv7m program. Note that the .w specifies "wide"
+// for the 32 bit version of the instruction
+//
+//
+//   0:   f04f 0000       mov.w   r0, #0
+//   4:   f380 8814       msr     CONTROL, r0
+//   8:   f3bf 8f6f       isb     sy
+//   c:   f06f 0e06       mvn.w   lr, #6
+//   10:   f3ef 8005       mrs     r0, IPSR
+//   14:   f000 00ff       and.w   r0, r0, #255    @ 0xff
+//   18:   f1a0 0010       sub.w   r0, r0, #16
+//   1c:   0942            lsrs    r2, r0, #5
+//   1e:   2301            movs    r3, #1
+//   20:   f000 001f       and.w   r0, r0, #31
+//   24:   fa03 f000       lsl.w   r0, r3, r0
+//   28:   4b03            ldr     r3, [pc, #12]   @ (38 <generic_isr_arm_v7m+0x38>)
+//   2a:   f843 0022       str.w   r0, [r3, r2, lsl #2]
+//   2e:   4b03            ldr     r3, [pc, #12]   @ (3c <generic_isr_arm_v7m+0x3c>)
+//   30:   f843 0022       str.w   r0, [r3, r2, lsl #2]
+//   34:   4770            bx      lr
+//   36:   0000            movs    r0, r0
+//   38:   e000e180        .word   0xe000e180
+//   3c:   e000e200        .word   0xe000e200
+//
+//
 pub fn generic_isr_armv7m(mut armv7m: Armv7m) {
     armv7m.mov(GeneralPurposeRegister::R0, Value::Value(0));
     armv7m.msr(
@@ -10,12 +37,13 @@ pub fn generic_isr_armv7m(mut armv7m: Armv7m) {
     );
     armv7m.isb();
     
-    // R14 is the link register
-    armv7m.ldr(GeneralPurposeRegister::R14, Value::Value(0xFFFFFFF9));
+    // r14 is the link register
+    armv7m.mvn(GeneralPurposeRegister::R14, Value::Value(6));
 
-    armv7m.mrs(GeneralPurposeRegister::R0, SpecialRegister::ISPR);
+    armv7m.mrs(GeneralPurposeRegister::R0, SpecialRegister::IPSR);
     armv7m.and(GeneralPurposeRegister::R0, Value::Value(0xFF), None);
     armv7m.sub(GeneralPurposeRegister::R0, Value::Value(16));
+    
 
     armv7m.lsrs(
         GeneralPurposeRegister::R2,
@@ -34,6 +62,8 @@ pub fn generic_isr_armv7m(mut armv7m: Armv7m) {
         Value::Register(GeneralPurposeRegister::R0),
     );
 
+    // VTOCK TODO: Encode this as the pc + 12 operation???
+    // Also, the PC has to move with instructions...
     armv7m.ldr(GeneralPurposeRegister::R3, Value::Value(0xe000e180));
 
     // VTOCK TODO - is it ok to just hard code 4 here - also I don't our encoding of this is right ????
@@ -47,6 +77,7 @@ pub fn generic_isr_armv7m(mut armv7m: Armv7m) {
         ],
     );
 
+    // VTOCK TODO: Encode this as the pc + 12 operation???
     armv7m.ldr(GeneralPurposeRegister::R3, Value::Value(0xe000e200));
 
     // VTOCK TODO - is it ok to just hard code 4 here - also I don't our encoding of this is right ????
