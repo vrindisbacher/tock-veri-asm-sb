893ea4142689cb37cf586789d412e4624b4b0a3d
vrindisbacher <vrindisbacher77@gmail.com>
2024-12-18 16:01:11 -0800

    add systick isr

--- src/armv7m/cpu/isr.rs
+++ src/armv7m/cpu/isr.rs
@@ -29,7 +29,7 @@ flux_rs::defs! {
 
 impl Armv7m {
     #[flux_rs::sig(
-        fn (self: &strg Armv7m[@old_cpu]) 
+        fn (self: &strg Armv7m[@old_cpu]) -> BV32[bv32(0xFFFF_FFF9)]
         requires 
             bv_uge(get_special_reg(ipsr(), old_cpu), bv32(16))
             &&
@@ -63,7 +63,7 @@ impl Armv7m {
             }
         }
     )]
-    pub fn generic_isr(&mut self) {
+    pub fn generic_isr(&mut self) -> BV32 {
         // r0 = 0
         self.movw_imm(GPR::R0, BV32::from(0));
         // control = r0 = 0
@@ -109,9 +109,53 @@ impl Armv7m {
         //
         // mem[0xe000_e200 + ((ipsr - 16 >> 5) << 2)] = (1 << ipsr - 16 & 31) i.e. "bit for the ipsr # is set"
         self.strw_lsl_reg(GPR::R0, GPR::R3, GPR::R2, BV32::from(2));
-        self.bx(SpecialRegister::Lr);
+        // self.bx(SpecialRegister::Lr);
+        return self.get_value_from_special_reg(&SpecialRegister::Lr);
     }
 
-    fn svc_isr(&mut self) {}
-    fn sys_tick_isr(&mut self) {}
+    fn svc_isr(&mut self) -> BV32 {
+        todo!()
+    }
+
+    // Use the CONTROL register to set the thread mode to privileged to switch
+    // back to kernel mode.
+    //
+    // CONTROL[1]: Stack status
+    //   0 = Default stack (MSP) is used
+    //   1 = Alternate stack is used
+    // CONTROL[0]: Mode
+    //   0 = Privileged in thread mode
+    //   1 = User state in thread mode
+    // mov r0, #0                        // r0 = 0
+    // msr CONTROL, r0                   // CONTROL = 0
+    // CONTROL writes must be followed by an Instruction Synchronization Barrier
+    // (ISB). https://developer.arm.com/documentation/dai0321/latest
+    // isb                               // synchronization barrier
+
+    // Set the link register to the special EXC_RETURN value of 0xFFFFFFF9 which
+    // instructs the CPU to run in thread mode with the main (kernel) stack.
+    // ldr lr, =0xFFFFFFF9               // LR = 0xFFFFFFF9
+
+    // This will resume in the switch_to_user function where application state
+    // is saved and the scheduler can choose what to do next.
+    // bx lr
+    #[flux_rs::sig(
+        fn (self: &strg Armv7m[@old_cpu]) -> BV32[bv32(0xFFFF_FFF9)]
+            requires mode_is_handler(old_cpu.mode)
+            ensures self: Armv7m { new_cpu: new_cpu == Armv7m {
+                    general_regs: map_set(old_cpu.general_regs, r0(), bv32(0)),
+                    control: Control { npriv: false, ..old_cpu.control },
+                    lr: bv32(0xFFFF_FFF9),
+                    ..old_cpu
+                }
+            }
+    )]
+    fn sys_tick_isr(&mut self) -> BV32 {
+        self.movw_imm(GPR::R0, BV32::from(0));
+        self.msr(SpecialRegister::Control, GPR::R0);
+        self.isb(Some(IsbOpt::Sys));
+        self.pseudo_ldr_special(SpecialRegister::Lr, BV32::from(0xFFFF_FFF9));
+        // self.bx(SpecialRegister::Lr);
+        return self.get_value_from_special_reg(&SpecialRegister::Lr);
+    }
 }
